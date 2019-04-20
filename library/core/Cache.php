e4d44e1a085c0fa5343b47fb18b6f0e13a159966
xwm <381345509@qq.com>
2019-04-20 21:15:26 +0800

    Make README.md prettier

--- library/core/Cache.php
+++ library/core/Cache.php
@@ -9,17 +9,18 @@
 abstract class Cache {
 
     public static function __callStatic($method, $params){
-        $redis = Pool::pop(Pool::TYPE_REDIS);
+        $key = Pool::TYPE_REDIS;
+        $redis = Pool::pop($key);
         if($redis){
             try{
                 return call_user_func_array([$redis, $method], $params);
             }catch(Exception $e){
-                Pool::destroy(Pool::TYPE_REDIS);
-                Pool::getInstance(Pool::TYPE_REDIS);
+                Pool::destroy($key);
+                Pool::getInstance($key);
             }
         }else{
-            Pool::destroy(Pool::TYPE_REDIS);
-            Pool::getInstance(Pool::TYPE_REDIS);
+            Pool::destroy($key);
+            Pool::getInstance($key);
         }
     }
 
