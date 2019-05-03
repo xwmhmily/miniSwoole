9611c860cff4a865bcf8a7bae693ad4f0827113c
xwm <381345509@qq.com>
2019-05-04 00:35:56 +0800

    Add http middleware

--- library/middleware/HttpMiddleware.php
+++ library/middleware/HttpMiddleware.php
@@ -2,18 +2,8 @@
 
 class HttpMiddleware {
 
-	private static function beforeHandle($method, swoole_http_request $request, swoole_http_response $response){
-		$response->write(__METHOD__.'<br />');
-	}
-
 	public static function handle($method, swoole_http_request $request, swoole_http_response $response){
-		self::beforeHandle($method, $request, $response);
-		$response->write(__METHOD__.'<br />');
-		self::afterHandle($method, $request, $response);
+		// $response->write(__METHOD__.'<br />');
 	}
 
-	private static function afterHandle($method, swoole_http_request $request, swoole_http_response $response){
-		$response->write(__METHOD__.'<br />');
-		$response->end(__FILE__.'<br />');
-	}
 }
