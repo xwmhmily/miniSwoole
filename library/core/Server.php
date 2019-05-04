984f53efe8e8b77eeb8fb38bc5bd6f86faab0a74
xwm <381345509@qq.com>
2019-05-04 23:16:47 +0800

    Improve MySQL/Redis connector

--- library/core/Server.php
+++ library/core/Server.php
@@ -9,8 +9,8 @@ abstract class Server {
 	public static $type;
 	public static $page;
 	public static $instance;
-	const OS_LINUX = 'LINUX';
-
+	
+	const OS_LINUX        = 'LINUX';
 	const TYPE_TCP        = 'tcp';
 	const TYPE_UDP        = 'udp';
 	const TYPE_HTTP       = 'http';
