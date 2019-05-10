1149e23196c67f1df275c18729790c32078428f5
xwm <381345509@qq.com>
2019-05-10 19:43:24 +0800

    Add server stat function

--- controller/Http.php
+++ controller/Http.php
@@ -10,6 +10,31 @@ class C_Http extends Controller {
         $this->m_news = $this->load('News');
     }
 
+    public function stat(){
+        $stat = [];
+        $stat['swoole_version'] = swoole_version();
+        $stat['masterPID'] = Server::$instance->master_pid;
+        $stat['server']    = Server::$type;
+
+        $ports = array(Server::$instance->ports)[0];
+        $ports_arr = [];
+        foreach($ports as $port){
+            $port = array($port);
+            foreach($port as $p){
+                $p = array($p)[0];
+                unset($p->setting);
+                $ports_arr[] = $p;
+            }
+        }
+
+        $stat['ports']   = $ports_arr;
+        $stat['stats']   = Server::$instance->stats();
+        $stat['setting'] = Server::$instance->setting;
+        $stat['config']  = include CONF_PATH.'/'.ENV.'.php';
+        $this->response->write(JSON($stat));
+        $this->response->end();
+    }
+
     public function log(){
         try{
             Logger::debug('This is a debug msg');
