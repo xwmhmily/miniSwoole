3c459e679e025195a5de13b81c43ab19501ce306
xwm <381345509@qq.com>
2019-05-13 14:59:57 +0800

    Improve getConfig() in Config.php

--- library/core/Config.php
+++ library/core/Config.php
@@ -6,21 +6,26 @@
 
 abstract class Config {
 
-	public static function get($section = '', $key = '') {
-		$config_file = CONF_PATH.'/'.ENV.'.php';
-		if(!file_exists($config_file)){
-			echo 'Error: config file '.$config_file.' NOT FOUND'.PHP_EOL; exit(0);
+	private static $config;
+
+	public static function getConfig($section = '', $key = '') {
+		if(!self::$config){
+			$config_file = CONF_PATH.'/'.ENV.'.php';
+			if(!file_exists($config_file)){
+				echo 'Error: config file '.$config_file.' NOT FOUND'.PHP_EOL; exit(0);
+			}
+
+			self::$config = include $config_file;
 		}
 
-		$config = include $config_file;
-		if($section){
-			if($key){
-				return $config[strtolower($section)][$key];	
-			}else{
-				return $config[strtolower($section)];
-			}
+		if(!$section){
+			return self::$config;
+		}
+		
+		if($key){
+			return self::$config[strtolower($section)][$key];
 		}else{
-			return $config;
+			return self::$config[strtolower($section)];
 		}
 	}
 }
