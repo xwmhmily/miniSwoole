5d622da9031acb78bd01c5e773bf417fa8e40b6c
xwm <381345509@qq.com>
2019-05-21 17:28:06 +0800

    Edit heartbeat.sh

--- shell/heartbeat.sh
+++ shell/heartbeat.sh
@@ -8,10 +8,11 @@ PID_FILE=../pid/swoole.pid
 SWOOLE_MASTER_PID=`cat $PID_FILE`
 NEW_SWOOLE_MASTER_PID=`ps -ef | grep ${SWOOLE_MASTER_PID} | grep -v "grep" | sed -n '1p' | awk -F ' ' '{print $2}'`
 
-cat logo.txt
+cd /usr/www/miniSwoole/shell
 
 if [ ! $NEW_SWOOLE_MASTER_PID ]; then
 	sh socket.sh restart
 else
+	cat logo.txt
 	echo -e "${GREEN_COLOR}Server with PID ${SWOOLE_MASTER_PID} is running ${RES}"
 fi
