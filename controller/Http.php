84375f032439d0a781b6af6b00b50682c1c2201e
xwm <381345509@qq.com>
2019-05-22 10:07:50 +0800

    Add try...catch in Stat controller

--- controller/Http.php
+++ controller/Http.php
@@ -10,30 +10,6 @@ class C_Http extends Controller {
         $this->m_news = $this->load('News');
     }
 
-    public function stat(){
-        $stat = [];
-        $stat['swoole_version'] = swoole_version();
-        $stat['server'] = Server::$type;
-        $ports = array(Server::$instance->ports)[0];
-        $ports_arr = [];
-        foreach($ports as $port){
-            $port = array($port);
-            foreach($port as $p){
-                $p = array($p)[0];
-                unset($p->setting);
-                $ports_arr[] = $p;
-            }
-        }
-
-        $stat['ports']   = $ports_arr;
-        $stat['stats']   = Server::$instance->stats();
-        $stat['setting'] = Server::$instance->setting;
-        $stat['config']  = include CONF_PATH.'/'.ENV.'.php';
-        $stat['masterPID'] = Server::$instance->master_pid;
-        $this->response->write(JSON($stat));
-        $this->response->end();
-    }
-
     public function log(){
         try{
             Logger::debug('This is a debug msg');
--- controller/Stat.php
+++ controller/Stat.php
@@ -3,37 +3,49 @@
 class C_Stat extends Controller{
 
     public function index(){
-        $stat = [];
-        $stat['swoole_version'] = swoole_version();
-        $stat['masterPID'] = Server::$instance->master_pid;
-        $stat['server']    = Server::$type;
+        try{
+            $stat = [];
+            $stat['swoole_version'] = swoole_version();
+            $stat['masterPID'] = Server::$instance->master_pid;
+            $stat['server']    = Server::$type;
 
-        $ports = array(Server::$instance->ports)[0];
-        $ports_arr = [];
-        foreach($ports as $port){
-            $port = array($port);
-            foreach($port as $p){
-                $p = array($p)[0];
-                unset($p->setting);
-                $ports_arr[] = $p;
+            $ports = array(Server::$instance->ports)[0];
+            $ports_arr = [];
+            foreach($ports as $port){
+                $port = array($port);
+                foreach($port as $p){
+                    $p = array($p)[0];
+                    unset($p->setting);
+                    $ports_arr[] = $p;
+                }
             }
-        }
 
-        $stat['ports']   = $ports_arr;
-        $stat['config']  = Config::get();
-        $stat['stats']   = Server::$instance->stats();
-        $stat['setting'] = Server::$instance->setting;
-        $this->response->write(JSON($stat));
-        $this->response->end();
+            $stat['ports']   = $ports_arr;
+            $stat['config']  = Config::get();
+            $stat['stats']   = Server::$instance->stats();
+            $stat['setting'] = Server::$instance->setting;
+            $this->response->write(JSON($stat));
+            $this->response->end();
+        }catch (Throwable $e){
+			$this->error($e);
+		}
     }
 
     public function ping(){
-        $this->response->end('PONG');
+        try{
+            $this->response->end('PONG');
+        }catch (Throwable $e){
+			$this->error($e);
+		}
     }
 
     public function process(){
-        $cmd = "ps -ef | grep Mini_Swoole | grep -v grep | awk -F ' ' '{print $2\"-\"$8}'";
-        exec($cmd, $retval, $execState);
-        $this->response->end(JSON($retval));
+        try{
+            $cmd = "ps -ef | grep Mini_Swoole | grep -v grep | awk -F ' ' '{print $2\"-\"$8}'";
+            exec($cmd, $retval, $execState);
+            $this->response->end(JSON($retval));
+        }catch (Throwable $e){
+			$this->error($e);
+		}
     }
 }
