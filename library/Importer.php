a30736338f8182a891462b7778ac7a9064ed193e
xwm <381345509@qq.com>
2019-05-30 10:16:38 +0800

    Edit README.md

--- library/Importer.php
+++ library/Importer.php
@@ -2,6 +2,10 @@
 
 class Importer {
 
+	public static function task(...$param){
+		Logger::log('Param in '.__METHOD__.' is => '.JSON($param));
+	}
+
 	public static function run(...$param){
 		Logger::log('Param in '.__METHOD__.' => '.JSON($param));
 	}
