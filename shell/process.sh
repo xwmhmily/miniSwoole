bd1dd5160b1de11f2538c4399727b1a53e72ffc7
xwmhmily@126.com <381345509@qq.com>
2020-03-21 14:52:42 +0800

    Add process control script

---
+++ shell/process.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Heartbeat the process
+# Usage: sh process.sh
+
+PHP=`which php`
+PARENT_PATH=$(dirname "$PWD")
+PROCESS_HEARTBEAT_FILE=$PARENT_PATH"/Heartbeat.php"
+
+cat logo.txt && $PHP $PROCESS_HEARTBEAT_FILE
